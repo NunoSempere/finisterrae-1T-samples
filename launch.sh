49d1484c3966728e6fc78f2da09a4f2668b64aae
Jorge Sierra <j.sierra.mn@gmail.com>
2024-10-06 15:07:24 +0300

    launch.sh changes

--- launch.sh
+++ launch.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #SBATCH -o samples-%J.out
 #SBATCH -e samples-%J.err
-# #SBATCH -C clk
 #SBATCH --time=0-06:00:00 #requested time to run the job
 #SBATCH -n 2 #(2 MPI processes)
 #SBATCH --ntasks-per-node=1 #(1 process per node, so 4 nodes)
-#SBATCH --cpus-per-task=64
-#SBATCH --mem 240GB
-#190GB
+#SBATCH --cpus-per-task=32
+#SBATCH --mem 40GB
 #SBATCH --mail-type=begin #Envía un correo cuando el trabajo inicia
 #SBATCH --mail-type=end #Envía un correo cuando el trabajo finaliza
-#SBATCH --mail-user=jorgesierra1998@gmail.com #Dirección a la que se envía
-module purge
-module load intel impi
+#SBATCH --mail-user=jorgesierra1998@gmail.com hello@nunosempere.com #Dirección a la que se envía
+
 make build
 
 srun ./samples
